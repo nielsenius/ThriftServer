3bd06d982dd7d770dcda5641e56efdcf2b06717d
Matthew Nielsen <mqnielsen@me.com>
2015-10-06 13:17:13 -0400

    Removed edit routes.

--- Thrift/urls.py
+++ Thrift/urls.py
@@ -2,10 +2,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^create_user/', 'ThriftServer.views.create_user'),
-    url(r'^edit_user/', 'ThriftServer.views.edit_user'),
     
     url(r'^create_item/', 'ThriftServer.views.create_item'),
-    url(r'^edit_item/', 'ThriftServer.views.edit_item'),
     url(r'^request_item/', 'ThriftServer.views.request_item'),
     url(r'^give_item/', 'ThriftServer.views.give_item'),
     
